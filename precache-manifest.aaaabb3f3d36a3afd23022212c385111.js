037308aec1794e3ba9d417442d8886581e837d54
Velpuri Akhil <akhilbuggati@gmail.com>
2021-10-04 19:10:54 +0530

    Updates

---
+++ precache-manifest.aaaabb3f3d36a3afd23022212c385111.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a551c3f0363c9dd8d7ad757bc88bcc4",
+    "url": "/Grocery-List/index.html"
+  },
+  {
+    "revision": "1a110f0fecf95e8e0673",
+    "url": "/Grocery-List/static/css/main.33f34963.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/Grocery-List/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Grocery-List/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a110f0fecf95e8e0673",
+    "url": "/Grocery-List/static/js/main.76cc9a2e.chunk.js"
+  },
+  {
+    "revision": "675809e6d4c5adb04a96",
+    "url": "/Grocery-List/static/js/runtime-main.6f15776a.js"
+  }
+]);
